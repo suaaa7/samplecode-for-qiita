9e8da81fc4f04f24ee1b667cd76c0862e51d988b
suaaa7 <spuaaa7@gmail.com>
2020-08-02 18:24:03 +0900

    Fixed bug

--- python_test_example/flask_app/app_test.py
+++ python_test_example/flask_app/app_test.py
@@ -19,15 +19,15 @@ class AppTestCase(TestCase):
         self.assertEqual(response.content_type, 'application/json')
 
     def test_predict(self):
-        response = self.client.get('/predict')
+        response = self.client.post('/predict')
         self.assertEqual(response.status_code, 200)
 
     def test_predict_content(self):
-        response = self.client.get('/predict')
+        response = self.client.post('/predict')
         self.assertEqual(response.content_type, 'application/json')
 
     def test_predict_data(self):
-        response = self.client.get('/predict')
+        response = self.client.post('/predict')
         self.assertIsInstance(json.loads(response.data)['result'], float)
 
 if __name__ == '__main__':
