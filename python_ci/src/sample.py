13c876ce8c63fe11660740189fbdc97ea7fb37a7
suaaa7 <spuaaa7@gmail.com>
2020-09-20 22:02:16 +0900

    Add non-public method to Sample

--- python_ci/src/sample.py
+++ python_ci/src/sample.py
@@ -3,5 +3,8 @@ import requests
 
 class Sample:
     def fetch_json(self, url: str) -> dict:
-        response = requests.get(url)
+        response = self._get_response(url)
         return response.json()
+
+    def _get_response(self, url: str) -> requests.models.Response:
+        return requests.get(url)
